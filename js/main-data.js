da332b5e33d267484fb7cdd2767af47c6b28928b
KhaetbekIT <hayotbek-web@mail.ru>
2023-07-30 22:13:38 +0500

    #fix #main-data #refactoring #html Фикс не нужного верстки и кода

--- js/main-data.js
+++ js/main-data.js
@@ -1,7 +1,82 @@
 const MainDataFunc = async () => {
 
+    const url = `https://anime-b7fa6-default-rtdb.firebaseio.com`
+
+    const RenderGanreList = (ganres) => {
+        const dropDownBlock = document.querySelector("#dropdown-js")
+
+        dropDownBlock.innerHTML = ""
+
+        ganres.forEach(ganre => {
+            dropDownBlock.insertAdjacentHTML("beforeend", `
+                <li><a href="./categories.html?ganre=${ganre}">${ganre}</a></li>
+            `)
+        })
+    }
+
     const RenderAnimeList = (array = [], ganres = {}) => {
-        console.log(array, ganres);
+        const container = document.querySelector("#product-container-js")
+
+        container.innerHTML = ""
+
+        ganres.forEach((ganre) => {
+            const productBlock = document.createElement('div')
+
+            const listBlock = document.createElement("div")
+
+            const list = array.filter(item => item.ganre === ganre)
+
+            listBlock.classList.add("row")
+
+            productBlock.classList.add("mb-5")
+
+            productBlock.insertAdjacentHTML("beforeend", `
+                <div class="row">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                        <div class="section-title">
+                            <h4>${ganre}</h4>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <div class="btn__all">
+                            <a href="./categories.html?ganre=${ganre}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                        </div>
+                    </div>
+                </div>
+            `)
+
+            list.forEach(item => {
+                const ulBlock = document.createElement("ul")
+
+                item.tags.forEach(tag => {
+                    ulBlock.insertAdjacentHTML("beforeend", `
+                        <li>${tag}</li>
+                    `)
+                })
+
+                listBlock.insertAdjacentHTML("beforeend", `
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="product__item">
+                            <div class="product__item__pic set-bg" data-setbg="${item.image}">
+                                <div class="ep">${item.rating} / 10</div>
+                                <div class="view"><i class="fa fa-eye"></i> ${item.views} </div>
+                            </div>
+                            <div class="product__item__text">
+                                ${ulBlock.outerHTML}
+                                <h5><a href="./anime-details.html?itemId=${item.id}">${item.title}</a>
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                `)
+            })
+
+            productBlock.append(listBlock)
+
+            container.append(productBlock)
+
+            container.querySelectorAll('.set-bg').forEach(element => element.style.backgroundImage = `url(${element.dataset.setbg})`)
+        })
     }
 
     const RenderTopANime = (array = []) => {
@@ -11,7 +86,7 @@ const MainDataFunc = async () => {
 
         array.forEach(item => {
             // day years
-            container.insertAdjacentHTML("afterbegin", `
+            container.insertAdjacentHTML("beforeend", `
                 <div class="product__sidebar__view__item set-bg mix day years"
                     data-setbg="${item.image}">
                     <div class="ep">${item.rating} / 10</div>
@@ -24,15 +99,17 @@ const MainDataFunc = async () => {
         container.querySelectorAll('.set-bg').forEach(element => element.style.backgroundImage = `url(${element.dataset.setbg})`)
     }
 
-    await fetch("https://anime-b7fa6-default-rtdb.firebaseio.com/anime.json").then(response => response.json()).then(data => {
+    await fetch(`${url}/anime.json`).then(response => response.json()).then(data => {
 
         const ganres = new Set()
 
-        RenderTopANime(data.sort((a, b) => b.views - a.views).slice(0, 5))
-
         data.forEach(item => ganres.add(item.ganre))
 
+        RenderTopANime(data.sort((a, b) => b.views - a.views).slice(0, 5))
+
         RenderAnimeList(data, ganres)
+
+        RenderGanreList(ganres)
     }).catch(error => console.error(error))
 }
 
