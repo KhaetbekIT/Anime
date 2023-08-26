275e8b3b3aec15d2039e315117fd9a2c79523f0a
KhaetbekIT <hayotbek-web@mail.ru>
2023-08-27 02:19:25 +0500

    #fix #js #JS Фикс путь до базы

--- js/main-data.js
+++ js/main-data.js
@@ -1,6 +1,6 @@
 const MainDataFunc = async () => {
 
-    const url = location.origin + "/db.json"
+    const url = "../db.json"
 
     const RenderGanreList = (ganres) => {
         const dropDownBlock = document.querySelector("#dropdown-js")
