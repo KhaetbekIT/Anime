e4efb1907dc4dadc0761bf6fb74e400496510fa5
KhaetbekIT <hayotbek-web@mail.ru>
2023-10-02 23:20:18 +0500

    #fix #js #html Фикс добавление данных

--- src/module/admin/render-lists.js
+++ src/module/admin/render-lists.js
@@ -1,4 +1,4 @@
-const RenderCategoriesFunc = (array = [], container, child = "") => {
+const RenderCategoriesFunc = (array = [], container) => {
     if (container) {
         container.innerHTML = ""
         array.forEach((item, index) => {
@@ -18,4 +18,35 @@ const RenderCategoriesFunc = (array = [], container, child = "") => {
     }
 }
 
-export { RenderCategoriesFunc }
+const RenderVideosFunc = (array = [], container) => {
+    if (container) {
+        container.innerHTML = ""
+        array.forEach((item, index) => {
+            index += 1
+            container.insertAdjacentHTML("beforeend", `
+                <tr>
+                    <th scope="row">${index}</th>
+                    <td>${item.ganre}</td>
+                    <td>${item.title}</td>
+                    <td> ${item.views} </td>
+                    <td class="text-end">
+                        <button type="button" class="btn btn-danger btn-sm" data-button-index="${item.id}">
+                            удалить
+                        </button>
+                    </td>
+                </tr>
+            `)
+        })
+    }
+}
+
+const RenderSelectFunc = (array = [], container) => {
+    if (container) {
+        container.innerHTML = `<option value="default" selected>Выберите категорию</option>`
+        array.forEach((item) => {
+            container.insertAdjacentHTML("beforeend", `<option value="${item.id}">${item.name}</option>`)
+        })
+    }
+}
+
+export { RenderCategoriesFunc, RenderVideosFunc, RenderSelectFunc }
--- src/module/admin/script.js
+++ src/module/admin/script.js
@@ -2,109 +2,291 @@ import axios from "axios";
 import { API } from "../api";
 import RenderGanreListFunc from "./categories-lists";
 import { DisbleFunc } from "../disable";
-import { RenderCategoriesFunc } from "./render-lists";
+import { RenderCategoriesFunc, RenderSelectFunc, RenderVideosFunc } from "./render-lists";
 
-const AdminScriptFunc = async () => {
+const AdminScriptFunc = () => {
 
-    const categories = document.querySelector("#category-wrapper-js")
-    const categoryTitle = categories.querySelector("#category-name")
-    const categoryImage = categories.querySelector("#category-image")
-    const categoryAddBtn = categories.querySelector("#category-add-btn")
+    const CategoryFunc = async () => {
+        const categories = document.querySelector("#category-wrapper-js")
+        const categoryTitle = categories.querySelector("#category-name")
+        const categoryImage = categories.querySelector("#category-image")
+        const categoryAddBtn = categories.querySelector("#category-add-btn")
 
-    const tableContainer = categories.querySelector("#category-container")
+        const tableContainer = categories.querySelector("#category-container")
 
-    const categoriesItem = {
-        name: null,
-        image: null
-    }
+        const select = document.querySelector("#video-category")
 
-    const inputArray = [categoryTitle, categoryImage]
-
-    DisbleFunc(inputArray, categoryAddBtn)
-
-    await axios.get(`${API}/anime`).then(response => {
-        const data = response.data;
-
-        const genres = new Set()
-
-        data.forEach(item => genres.add(item.ganre));
-
-        RenderGanreListFunc(genres)
-    })
-
-    categoryTitle?.addEventListener("input", () => {
-        DisbleFunc(inputArray, categoryAddBtn)
-
-        categoriesItem.name = categoryTitle?.value
-    })
-
-    categoryImage?.addEventListener("input", () => {
-        DisbleFunc(inputArray, categoryAddBtn)
-
-        const fileReader = new FileReader()
-
-        const file = categoryImage.files[0]
-
-        if (
-            file.type === "image/jpeg" ||
-            file.type === "image/jpg" ||
-            file.type === "image/png" ||
-            file.type === "image/webp" ||
-            file.type === "image/svg" ||
-            file.type === "image/jfif"
-        ) {
-            fileReader.addEventListener("load", () => {
-                categoriesItem.image = fileReader.result
-            })
-
-            fileReader.addEventListener("error", () => {
-                categoriesItem.image = null
-                categoryImage.value = ""
-                alert(`ERROR: error onload file. Try again!`)
-            })
-
-            fileReader.readAsDataURL(file)
-
-        } else {
-            alert(`TYPE FILE IS NOT CORRECT`)
-            categoryImage.value = ""
+        const categoriesItem = {
+            name: null,
+            image: null
         }
-    })
 
-    axios.get(`${API}/categories/`).then(response => {
-        const data = response.data;
-        RenderCategoriesFunc(data, tableContainer)
-    })
+        const inputArray = [categoryTitle, categoryImage]
 
-    categoryAddBtn?.addEventListener("click", () => {
-        axios.post(`${API}/categories/`, categoriesItem).then(() => {
-            alert(`CATEGORY IS ADDED`)
-            categoryTitle.value = ""
-            categoryImage.value = ""
+        DisbleFunc(inputArray, categoryAddBtn)
 
-            axios.get(`${API}/categories/`).then(response => {
-                const data = response.data;
-                RenderCategoriesFunc(data, tableContainer)
-            })
+        await axios.get(`${API}/categories/`).then(response => {
+            const data = response.data;
+            RenderSelectFunc(data, select)
         })
-    })
 
-    tableContainer?.addEventListener("click", e => {
-        if (e.target && e.target.tagName === "BUTTON") {
-            const target = e.target
+        await axios.get(`${API}/anime`).then(response => {
+            const data = response.data;
 
-            const deleteIndex = target.dataset.buttonIndex;
+            const genres = new Set()
 
-            axios.delete(`${API}/categories/${deleteIndex}`).then(() => {
-                alert(`CATEGORY WAS DELETED`);
+            data.forEach(item => genres.add(item.ganre));
 
-                axios.get(`${API}/categories/`).then(response => {
+            RenderGanreListFunc(genres)
+        })
+
+        categoryTitle?.addEventListener("input", () => {
+            DisbleFunc(inputArray, categoryAddBtn)
+
+            categoriesItem.name = categoryTitle?.value
+        })
+
+        categoryImage?.addEventListener("input", () => {
+            DisbleFunc(inputArray, categoryAddBtn)
+
+            const fileReader = new FileReader()
+
+            const file = categoryImage.files[0]
+
+            if (
+                file.type === "image/jpeg" ||
+                file.type === "image/jpg" ||
+                file.type === "image/png" ||
+                file.type === "image/webp" ||
+                file.type === "image/svg" ||
+                file.type === "image/jfif"
+            ) {
+                fileReader.addEventListener("load", () => {
+                    categoriesItem.image = fileReader.result
+                })
+
+                fileReader.addEventListener("error", () => {
+                    categoriesItem.image = null
+                    categoryImage.value = ""
+                    alert(`ERROR: error onload file. Try again!`)
+                })
+
+                fileReader.readAsDataURL(file)
+
+            } else {
+                alert(`TYPE FILE IS NOT CORRECT`)
+                categoryImage.value = ""
+            }
+        })
+
+        await axios.get(`${API}/categories/`).then(response => {
+            const data = response.data;
+            RenderCategoriesFunc(data, tableContainer)
+        })
+
+        categoryAddBtn?.addEventListener("click", () => {
+            axios.post(`${API}/categories/`, categoriesItem).then(async () => {
+                alert(`CATEGORY IS ADDED`)
+                categoryTitle.value = ""
+                categoryImage.value = ""
+
+                await axios.get(`${API}/categories/`).then(response => {
                     const data = response.data;
                     RenderCategoriesFunc(data, tableContainer)
                 })
-            }).catch(()=> alert("ERROR: error from delete"))
+
+                await axios.get(`${API}/categories/`).then(response => {
+                    const data = response.data;
+                    RenderSelectFunc(data, select)
+                })
+            })
+        })
+
+        tableContainer?.addEventListener("click", e => {
+            if (e.target && e.target.tagName === "BUTTON") {
+                const target = e.target
+
+                const deleteIndex = target.dataset.buttonIndex;
+
+                axios.delete(`${API}/categories/${deleteIndex}`).then(() => {
+                    alert(`CATEGORY WAS DELETED`);
+
+                    axios.get(`${API}/categories/`).then(response => {
+                        const data = response.data;
+                        RenderCategoriesFunc(data, tableContainer)
+                    })
+
+
+                }).catch(() => alert("ERROR: error from delete"))
+            }
+        })
+    }
+
+    const VideosListFunc = () => {
+        const videosWrapper = document.querySelector("#video-wrapper-js")
+        const videoContainer = videosWrapper.querySelector("#video-container")
+        const form = videosWrapper.querySelector("#form-js")
+        const select = form[0]
+        const image = form[1]
+        const title = form[2]
+        const originalTitle = form[3]
+        const tags = form[4]
+        const year = form[5]
+        const rating = form[6]
+        const videoFile = form[7]
+        const desc = form[8]
+        const addVideoBtn = form[9]
+
+        const animeItem = {
+            image: undefined,
+            title: null,
+            "original-title": null,
+            ganre: null,
+            tags: [],
+            date: `0000`,
+            views: 0,
+            rating: 0,
+            description: "",
+            video: undefined
         }
-    })
+
+        const formValues = [select, image, title, originalTitle, tags, year, rating, videoFile, desc]
+
+        form?.addEventListener("submit", e => {
+            e.preventDefault()
+        })
+
+        DisbleFunc(formValues, addVideoBtn)
+
+        select?.addEventListener("change", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            if (select?.value !== "default") {
+                axios.get(`${API}/categories/${select.value}`).then(response => {
+                    const data = response.data;
+                    animeItem.ganre = data.name;
+                })
+            }
+        })
+
+        image?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+            const fileReader = new FileReader()
+
+            const file = image.files[0]
+
+            if (
+                file.type === "image/jpeg" ||
+                file.type === "image/jpg" ||
+                file.type === "image/png" ||
+                file.type === "image/webp" ||
+                file.type === "image/svg" ||
+                file.type === "image/jfif"
+            ) {
+                fileReader.addEventListener("load", () => {
+                    animeItem.image = fileReader.result
+                })
+
+                fileReader.addEventListener("error", () => {
+                    animeItem.image = null
+                    image.value = ""
+                    alert(`ERROR: error onload file. Try again!`)
+                })
+
+                fileReader.readAsDataURL(file)
+
+            } else {
+                alert(`TYPE FILE IS NOT CORRECT`)
+                image.value = ""
+            }
+        })
+
+        title?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem.title = title.value
+
+        })
+
+        originalTitle?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem["original-title"] = originalTitle.value
+        })
+
+        tags?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem.tags = tags.value.split(" ")
+        })
+
+        year?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem.date = year.value
+        })
+
+        rating?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem.rating = rating.value
+        })
+
+        videoFile?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+            const fileReader = new FileReader()
+
+            const file = videoFile.files[0]
+
+            if (
+                file.type === "video/mp4"
+            ) {
+                fileReader.addEventListener("load", () => {
+                    animeItem.video = fileReader.result
+                })
+
+                fileReader.addEventListener("error", () => {
+                    animeItem.video = undefined
+                    videoFile.value = ""
+                    alert(`ERROR: error onload file. Try again!`)
+                })
+
+                fileReader.readAsDataURL(file)
+
+            } else {
+                alert(`TYPE FILE IS NOT CORRECT`)
+                videoFile.value = ""
+            }
+        })
+
+        desc?.addEventListener("input", () => {
+            DisbleFunc(formValues, addVideoBtn)
+
+            animeItem.description = desc.value
+        })
+
+        addVideoBtn?.addEventListener("click", () => {
+            axios.post(`${API}/anime/`, animeItem).then(() => {
+                alert(`VIDEO WAS ADDED`)
+
+                axios.get(`${API}/anime/`).then(response => {
+                    const data = response.data;
+        
+                    RenderVideosFunc(data, videoContainer)
+                })
+            }).catch(()=> alert(`ERROR: error on adding video`))
+        })
+
+        axios.get(`${API}/anime/`).then(response => {
+            const data = response.data;
+
+            RenderVideosFunc(data, videoContainer)
+        })
+    }
+
+    CategoryFunc()
+
+    VideosListFunc()
 }
 
 export default AdminScriptFunc;
